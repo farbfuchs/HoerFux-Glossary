8d38fdb4bef5c99c41ee4210003e330ce45c9501
klengelci <cigdem.klengel@ukdd.de>
2024-05-05 19:14:15 +0200

    .

--- yaml_reader.py
+++ yaml_reader.py
@@ -2,6 +2,7 @@ import yaml
 import sys
 import random
 
+"""Helper function to load YAML data from file."""
 def load_yaml(path):
     try:
         with open(path, 'r') as file:
@@ -12,24 +13,29 @@ def load_yaml(path):
         sys.exit()
     except yaml.YAMLError as error:
         print("YAML parsing error:", error)
-        sys.exit()    
+        sys.exit() 
 
+"""Main entry point of the program."""
 def main():
     path = "./glossary.yml"
     data = load_yaml(path)
-    randomKey = random.choice(list(data.keys()))
-    randomValue = data[randomKey]
-    print("Welcome Text to the HoerFux-Glossary \n The entry of the day is \n {} -> {} \n".format(randomKey, randomValue))
-    seeAll = input("Do you want to see all words in the HoerFux-Glossary (y/n)")
-    
-    if seeAll == 'y':
+
+    # Random selection
+    random_key = random.choice(list(data.keys()))
+    random_value = data[random_key]
+    print("Welcome to the HoerFux-Glossary \n \n Random selection from glossary \n {} -> {} \n".format(random_key, random_value))
+
+    # See all values
+    see_all = input("Do you want to see all words in the HoerFux-Glossary (y/n)")    
+    if see_all == 'y':
         print("All words in glossary: \n")
         [print(keys) for keys in data]
         print("\n")
 
-    askGlossary = input("Would you like to see the meaning of a word in the HoerFux-Glossary (y/n)?")    
+    ask_glossary = input("Would you like to see the meaning of a word in the HoerFux-Glossary (y/n)?")    
     
-    while askGlossary == 'y':
+    # Loop for input
+    while ask_glossary == 'y':
         key = input("Enter a word: ")
         if key in data:
             value = data[key]
@@ -37,8 +43,9 @@ def main():
         else:
             print("Word not found!")
 
-        askGlossary = input("Do you want to enter another word (y/n)?")
+        ask_glossary = input("Do you want to enter another word (y/n)?")
 
+    print("Thank you for using HoerFux-Glossary!")
     
 
 if __name__ == "__main__":
